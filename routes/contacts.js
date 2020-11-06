657526abcdf4200487625211ad14190696fb80d2
Afej <afekurojoshua@gmail.com>
2020-11-06 16:07:38 +0100

    finished backend setup and init react

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,32 +1,70 @@
 const express = require("express");
-
 const router = express.Router();
+const auth = require("../middleware/auth");
+const { body, validationResult } = require("express-validator");
+
+const User = require("../models/User");
+const Contact = require("../models/Contact");
 
 // @route   GET api/contacts
 // @desc   get all users contacts
 // @access   private
-router.get("/", (req, res) => {
-  res.send("get all conatcs");
+router.get("/", auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
 });
 
 // @route   POST api/contacts
 // @desc   add new contact
 // @access   private
-router.post("/", (req, res) => {
-  res.send("add new contacts");
-});
+router.post(
+  "/",
+  [auth, [body("name", "Name is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id,
+      });
+
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 // @route   PUT api/contacts/:id
 // @desc   update contact
 // @access   private
-router.put("/", (req, res) => {
+router.put("/", auth, (req, res) => {
   res.send("update conatcs");
 });
 
 // @route   DELETE api/contacts
 // @desc   delete contacts
 // @access   private
-router.delete("/", (req, res) => {
+router.delete("/", auth, (req, res) => {
   res.send("delete conatcs");
 });
 
